67e5c52b6fee1005fe56de96ecdd560c87aa7ee5
kbszabolcs <kozmabszabolcs@gmail.com>
2022-08-02 13:46:50 +0200

    Orders, users added

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using travelo_auth.Models;
+
+[Route("api/users")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public UsersController(UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    [HttpGet]
+    public ActionResult<ApplicationUser> GetUsers(){
+        return Ok(_userManager.Users);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<ApplicationUser> GetUserById(Guid userGuid){
+        var user = _userManager.FindByIdAsync(userGuid.ToString());
+        return Ok(user);
+    }
+}
