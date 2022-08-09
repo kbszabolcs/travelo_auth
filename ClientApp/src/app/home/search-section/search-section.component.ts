6c4ee378595606178c74df1f88f94d964713057a
kbszabolcs <kozmabszabolcs@gmail.com>
2022-08-09 11:38:26 +0200

    search-section selector added

--- ClientApp/src/app/home/search-section/search-section.component.ts
+++ ClientApp/src/app/home/search-section/search-section.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { Trip } from 'src/app/data/models/Trip';
 
 @Component({
   selector: 'app-search-section',
@@ -11,9 +13,9 @@ export class SearchSectionComponent {
   private checkInDate: Date = null;
   private checkOutDate: Date = null;
 
-  toppings = new FormControl('');
-  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
-
+  private tripLocations = new FormControl('');
+  private tripNames: string[] = [];
+  
   private listSearchResult(){
     console.log(this.checkInDate);
     console.log(this.checkOutDate);
